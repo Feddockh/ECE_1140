0958119a1d9490fd26dde763d109d6ab698f7da9
gjdeller <dellergarrett@gmail.com>
2024-06-16 15:12:11 -0400

    Updated: Blue Line Track Switch PLC

--- train_system/track_controller/HW_PLC.py
+++ train_system/track_controller/HW_PLC.py
@@ -1,33 +1,31 @@
 # train_system/track_controller/HW_PLC.py
 class HWPLC:
-    def __init__(self, block_array):
+    def __init__(self, block_array,authority):
         """
         Initialize Hardware Track Controller. Initializes the block_array (list of bools that are the track occupancies) Calls the check_occupaancies funcrtion
         """
+        self.authority = authority
         self.block_array = block_array
         self.occupancy_var = False
+        self.stationB_light = False
+        self.stationC_light = False
         self.check_occupancies()
-        self.close_crossing()
-    
+        #self.close_crossing()
+        
     def check_occupancies(self):
         """
-        Check and print occupancy status of each block
-
+        Check and print occupancy status of each block, then call the switch position function to determine if we should switch
         """
         num_blocks = len(self.block_array)
         for i in range(num_blocks):
             if self.block_array[i]:
-                print(f"Block {i} is occupied.")
-            else:
-                print(f"Block {i} is not occupied.")
+                self.switch_positions()
         
-       
-
     def close_crossing(self):
         """
         Check if adjacent blocks are occupied to decide closing the crossing gate.   Prints out whether the gate is closing or not.
         """
-        occupancy_var = False
+        self.occupancy_var = False
         length_array = len(self.block_array)
         for i in range(length_array):
             if (self.block_array[i]) or ( self.block_array[i-1]) or (i < length_array - 1 and self.block_array[i+1]):
@@ -37,13 +35,12 @@ class HWPLC:
             else:
                 print(f"Opening Crossing Gate, Pedestrians Free To Cross.")
                 self.occupancy_var = False
-            
-        self.light_signals()
 
+        self.light_signals()
 
     def light_signals(self):
         """
-        In this function, based on occupancies, we will determine if it is safe for people to cross
+        In this function, based on occupancies, this function will be used to determine light signals RED = Occupied, stop... GREEN = Unoccupied, GO
         """
         light_color = ""
         if self.occupancy_var:
@@ -56,14 +53,56 @@ class HWPLC:
     def switch_positions(self):
         """
         This is the function for switching the actual switches.
-        Going to take the 
+        Going to use the occupancies and check at the switch point if any of block 11 or 6 are occupied
+        If both are occupied, all trains will stop. 
         """
 
 
+        TrackSwitch = "ON"
+
+        if self.block_array[6] and not self.block_array[11]:
+            
+            print(f"Switch from Block 5 to 11 is {TrackSwitch}")
+            print(f"Traveling to Station C, Authority: {self.authority} miles per hour")
+
+        if self.block_array[11] and not self.block_array[6]:
+            
+            print(f"Switch from Block 5 to 6 is {TrackSwitch}")
+            print(f"Traveling to Staton B, Authority: {self.authority} miles per hour")
+        
+        else:
+            self.emergency_stop()
+
+    def emergency_stop(self):
+        """
+        E-STOP Function
+        Goal:
+            set authority to zero and lights to red
+        """
+        self.authority = 0
+        print(f"EMERGENCY STOP! STOPPING ALL TRAINS!")
+        
+
 # Test main function
 def main():
-    block_array = [True, False, False, False, False]
-    plc = HWPLC(block_array)
+    """
+    Test Bench
+    """
+
+    authority = 45
+    #            station   1      2      3     4       5      6      7      8      9     10      11     12     13     14     15
+    blue_line1 = [False, False, False, False, False, False, False, False, False, False, False, True, False, False, False, False ]
+    print(f"Blue Line Instance 1")
+    plc = HWPLC(blue_line1, authority)
+
+    blue_line2 = [False, False, False, False, False, False, True, False, False, False, False, False, False, False, False, False ]
+    print(f"Blue Line Instance 2")
+    plc2 = HWPLC(blue_line2, authority)
+
+    blue_line3 = [False, False, False, False, False, False, True, False, False, False, False, True, False, False, False, False ]
+    print(f"Blue Line Instance 3")
+    plc3 = HWPLC(blue_line2, authority)
+
 
 if __name__ == "__main__":
     main()
