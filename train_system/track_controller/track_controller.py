f14de4a1cd7d32f94a0adb93b918357ce30dc027
gjdeller <dellergarrett@gmail.com>
2024-06-16 10:17:02 -0400

    TrackControllerUpdates-6_16_2024

--- train_system/track_controller/track_controller.py
+++ train_system/track_controller/track_controller.py
@@ -1,2 +1,33 @@
-x = 'hello world'
-print(x) #hello world
+
+#Track Controller
+from ..common import TrackBlock
+
+class TrackController:
+    def __init__(self):
+        """
+        Initialize the Track Controller.
+        """
+
+        self.track_occupancies = {}
+        self.train_suggested_speeds = {}
+        self.train_authorities = {}
+    
+    def compute_suggested_speed(self):
+        """
+        Compute the suggested speed for each train based on track occupancy.
+
+        Returns:
+            float: Suggested speed for the train.
+        """
+    
+    def compute_authority(self, train_id: int):
+        """
+        Compute the authority for each train.
+        
+        Args:
+            train_id (int): Identifier for the train.
+        
+        Returns:
+            int: Authority for the train.
+        """
+    
