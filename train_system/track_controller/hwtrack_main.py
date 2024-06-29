fa74a869a50a250286e6b32be98a4b74c0f3e192
gjdeller <dellergarrett@gmail.com>
2024-06-29 19:34:42 -0400

    small changes

--- train_system/track_controller/hwtrack_main.py
+++ train_system/track_controller/hwtrack_main.py
@@ -10,7 +10,6 @@ def main():
     print(f"Blue Line Example 1")
       #                  yard   1      2      3     4       5      6      7      8      9     10      11     12     13     14     15
     track_occupancies = [False, False, False, False, False, False, False, False, True, False, False, False, False, False, False, False ]
-    
     authority = 45
     instance1 = HWTrackController(track_occupancies, authority)
 
